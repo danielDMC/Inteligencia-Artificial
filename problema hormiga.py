1aa3b76a1b7e622a13ef0ce88fd174927dbf2f5c
danielDMC <danieldmaria@gmail.com>
2016-08-11 17:56:55 -0500

    Add files via upload
    Subo el problema de la hormiga inconcluso, sólo es lo que trabajé durante la hora de clase! saludos

---
+++ problema hormiga.py
@@ -0,0 +1,20 @@
+arr = [
+[5,2,3,1,3],
+[1,2,3,3,4],
+[1,1,2,3,2],
+[0,5,1,2,2]
+
+]
+#print arr[0]
+
+
+eje_y= len(arr)-1
+eje_x= len (arr[0])-1
+tamanio = (eje_x+1)*(eje_y+1)
+
+
+print "x = "+str(eje_x)
+print "y = "+str(eje_y)
+print "tamaño arreglo = "+str(tamanio)
+
+
